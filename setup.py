b0f3d383833387b60fa5d169c6031c0da356f64e
djungelorm <djungelorm@users.noreply.github.com>
2017-06-30 17:48:40 +0100

    v0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinx-csharp',
-    version = '0.1.4',
+    version = '0.1.5',
     author = 'djungelorm',
     author_email = 'djungelorm@users.noreply.github.com',
     packages = ['sphinx_csharp'],
