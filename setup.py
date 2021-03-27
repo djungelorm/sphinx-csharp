8978b50646758932c604daaee84ab89da3bbe608
Roger Barton <rbarton@student.ethz.ch>
2021-03-27 11:14:22 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'sphinx-csharp',
-    version = '0.1.8',
+    version = '0.1.9',
     author = 'djungelorm',
     author_email = 'djungelorm@users.noreply.github.com',
     packages = ['sphinx_csharp'],
